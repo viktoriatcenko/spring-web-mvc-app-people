1fe9618d9675202d30272d104c46d996e1ca2425
yatsenko.v <FDT3sRtwD>
2024-04-23 20:53:19 +0300

    JDBC API - statement

--- src/main/java/ru/maxima/dao/PersonDAO.java
+++ src/main/java/ru/maxima/dao/PersonDAO.java
@@ -2,55 +2,147 @@ package ru.maxima.dao;
 
 // DAO - Data Access Object
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import ru.maxima.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class PersonDAO {
 
-    private Long PEOPLE_COUNT = 0L;
-    private List<Person> people;
+    private final String URL = "jdbc:postgresql://localhost:5433/maxima";
+    private final String USERNAME = "postgres";
+    private final String PASSWORD = "postgres";
 
-    // int a = 5;
-    int a;
+    private Connection connection;
 
     {
-        a = 5;
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Sergey", "sergey@mail.ru", 25)); // age, nationality, sex
-        people.add(new Person(++PEOPLE_COUNT, "Pavel","pavel@mail.ru", 25)); // 30, RF, -
-        people.add(new Person(++PEOPLE_COUNT, "Tagir", "tagir@mail.ru", 25));
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+
+
     public List<Person> getAllPeople() {
+
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQLQuery = "select * " +
+                    "from person";
+
+            ResultSet resultSet = statement.executeQuery(SQLQuery);
+
+            while (resultSet.next()) {
+                Person person = new Person();
+
+                person.setId(resultSet.getLong("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+
+                people.add(person);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
         return people;
     }
 
 
     public Person findById(Long id) {
-        return people.stream()
-                .filter(p -> p.getId().equals(id))
-                .findAny()
-                .orElse(null);
+        Person person = new Person();
+        try {
+            Statement statement = connection.createStatement();
+            // select * from person where id = 3
+            String SQLQuery = "select * " +
+                    "from person where id = " + id;
+            ResultSet resultSet = statement.executeQuery(SQLQuery);
+
+            while (resultSet.next()) {
+                person.setId(resultSet.getLong("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return person;
     }
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        Optional<Long> last = getAllPeople().stream()
+                .map(Person::getId)
+                .max(Comparator.naturalOrder());
+        Long id = last.get();
+
+        try {
+            Statement statement = connection.createStatement();
+            // insert into person (id, name, email, age)
+            //            values (1, 'Viktor', 'viktor@mail.ru', 33)
+
+            String SQLQuery = "insert into person (id, name, email, age) values (" + ++id +  ", '"
+                    + person.getName() + "', '" + person.getEmail() + "', " + person.getAge() + ") ";
+
+            statement.executeUpdate(SQLQuery);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public void update(Person personFromView, Integer id) {
-        Person toBeUpdated = findById(Long.valueOf(id));
-        toBeUpdated.setName(personFromView.getName());
-        toBeUpdated.setEmail(personFromView.getEmail());
-        toBeUpdated.setAge(personFromView.getAge());
+        // update person set name = '', age = 255, email = '' where id = 1;
+
+        try {
+            Statement statement = connection.createStatement();
+
+            String SQLQuery = "update person set name = '" + personFromView.getName() + "' ," +
+                    "age = " + personFromView.getAge() + ",  email = '" + personFromView.getEmail() +
+                    "'  where id = " + id;
+
+            statement.executeUpdate(SQLQuery);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public void delete(Integer id) {
-        people.removeIf(p -> p.getId().equals(Long.valueOf(id)));
+        // delete from person where id = 1;
+
+        try {
+            Statement statement = connection.createStatement();
+
+            String SQLQuery = "delete from person where id = " + id;
+
+            statement.executeUpdate(SQLQuery);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
